fdbdf7ddce963b78dfdb62fb0961c9790f1b26b5
pabloq-meli <pablo.quagliata@mercadolibre.com>
2021-09-13 11:41:50 -0300

    Integrador finished (#1)
    Co-authored-by: manu-alderete <manuel.alderete@mercadolibre.com>

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -1,3 +1,155 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// MARK: AlkeParking
+
+protocol Parkable {
+    var plate: String { get }
+    var type: VehicleType { get }
+    var checkInTime: Date { get set }
+    var discountCard: String? { get set }
+    var parkedTime: Int { get }
+}
+
+struct Parking {
+    var vehicles: Set<Vehicle> = []
+    let maximumVehicles = 20
+    
+    var total: (Int, Int) = (0,0)
+    
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish: (Bool) -> Void) {
+        onFinish(vehicles.count < maximumVehicles && vehicles.insert(vehicle).inserted)
+    }
+    
+    mutating func checkOutVehicle(_ plate: String, onSuccess: ((Int)->Void), onError: (()->Void)) {
+        guard let vehicle = vehicles.first(where: { $0.plate == plate }) else {
+            onError()
+            return
+        }
+        let fee = calculateFee(vehicle: vehicle)
+        vehicles.remove(vehicle)
+        total.0 += 1
+        total.1 += fee
+        onSuccess(fee)
+    }
+    
+    private func calculateFee(vehicle: Vehicle) -> Int {
+        // Luego de las 2 primeras horas se cobrarán $5 por cada 15 minutos o fracción.
+        var fee = Double(vehicle.type.parkingRate)
+        if(vehicle.parkedTime > 120) {
+            let additional = ((Double(vehicle.parkedTime) - 120) / 15) * 5
+            fee += additional
+        }
+        return Int((fee * (vehicle.discountCard != nil ? 0.85 : 1)).rounded(.up))
+    }
+    
+    func showTotalEarnings() {
+        print("\(total.0) vehicles have checked out and have earnings of \(total.1)")
+    }
+    
+    func showActiveVehicles() {
+        if(!vehicles.isEmpty) {
+            print("Active vehicles")
+            vehicles.forEach { print($0.plate) }
+        } else {
+            print("No active vehicles")
+        }
+    }
+}
+
+struct Vehicle: Parkable, Hashable {
+    let plate: String
+    let type: VehicleType // constante ya que un tipo de vehiculo no cambia con el tiempo
+    var checkInTime: Date
+    var discountCard: String?
+    var parkedTime: Int {
+        get {
+            return Calendar.current.dateComponents([.minute], from:
+                                                    checkInTime, to: Date()).minute ?? 0
+        }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(plate)
+    }
+    
+    static func == (lhs: Vehicle, rhs: Vehicle) -> Bool {
+        return lhs.plate == rhs.plate
+    }
+}
+
+enum VehicleType {
+    case bus
+    case car
+    case miniBus
+    case motorcycle
+    
+    var parkingRate: Int {
+        switch self {
+        case .bus:
+            return 30
+        case .car:
+            return 20
+        case .miniBus:
+            return 25
+        case .motorcycle:
+            return 15
+        }
+    }
+}
+
+// MARK: - Prueba
+
+var alkeParking = Parking()
+
+// uno repetido y uno que no se agrega por tamaño
+let vehicles = [
+    Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard:
+                "DISCOUNT_CARD_001"),
+    Vehicle(plate: "B222BBB", type: VehicleType.motorcycle, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard:
+                nil),
+    Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard:
+                "DISCOUNT_CARD_002"),
+    Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard:
+                "DISCOUNT_CARD_002"),
+    Vehicle(plate: "AA111BB", type: VehicleType.car, checkInTime: Date(), discountCard:
+                "DISCOUNT_CARD_003"),
+    Vehicle(plate: "B222CCC", type: VehicleType.motorcycle, checkInTime: Date(), discountCard:
+                "DISCOUNT_CARD_004"),
+    Vehicle(plate: "CC333DD", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "DD444EE", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_005"),
+    Vehicle(plate: "AA111CC", type: VehicleType.car, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "B222DDD", type: VehicleType.motorcycle, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333EE", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "DD444GG", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_006"),
+    Vehicle(plate: "AA111DD", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_007"),
+    Vehicle(plate: "B222EEE", type: VehicleType.motorcycle, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333FF", type: VehicleType.miniBus, checkInTime: Date(), discountCard:
+                nil),
+    Vehicle(plate: "XX333EE", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "YY444GG", type: VehicleType.bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_006"),
+    Vehicle(plate: "ZZ111DD", type: VehicleType.car, checkInTime: Date(), discountCard: "DISCOUNT_CARD_007"),
+    Vehicle(plate: "WW22EEE", type: VehicleType.motorcycle, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "RR333FF", type: VehicleType.miniBus, checkInTime: Date(), discountCard:
+                nil),
+    Vehicle(plate: "RR333GG", type: VehicleType.miniBus, checkInTime: Date(), discountCard:
+                nil)
+]
+
+vehicles.forEach { vehicle in
+    alkeParking.checkInVehicle(vehicle) { result in
+        print(result ? "Welcome to AlkeParking!" : "Sorry, the check-in failed")
+    }
+}
+
+alkeParking.showActiveVehicles()
+
+vehicles.forEach { vehicle in
+    alkeParking.checkOutVehicle(vehicle.plate) { fee in
+        print("Your vehicle \(vehicle.plate) has a fee of $\(fee). Come back soon")
+    } onError: {
+        print("Sorry, the check-out failed")
+    }
+}
+alkeParking.showTotalEarnings()
+alkeParking.showActiveVehicles()
